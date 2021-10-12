63323ffb9cb69a67b5a7f29f3286a62145793d14
Jason <jixiang.mrd@gmail.com>
2021-10-12 09:01:46 -0600

    first commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require dirname(__DIR__)."/vendor/autoload.php";
+use \Jx\Ctest\test\Test;
+
+echo "Index Page";
+
+echo "<br/>";
+
+Test::sayHi();
---
+++ src/test/Test.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Jx\Ctest\test;
+
+
+class Test{
+
+	public static function sayHi(){
+		echo "Test Class is saying hi";
+	}
+
+}
